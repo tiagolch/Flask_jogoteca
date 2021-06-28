c52b7a8bdd51a834e6d4b4c4f43163c7b62ceb38
m2b-tiagochaves <tiagodevch@gmail.com>
2021-06-28 10:10:25 -0300

    Adicionado requirements.txt diretorio static e templates/login.html

--- tasks.py
+++ tasks.py
@@ -1,13 +1,54 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 
 app = Flask(__name__)
+app.secret_key='tiagochaves'
+
+
+class User:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+
+tiago = User('admin', 'Tiago Chaves', 'admin')
+usuarios = {
+    tiago.id: tiago
+}
+
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + 'logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+        else:
+            flash('Não logado, tente de novo!')
+            return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Deslogado com sucesso.')
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
     return render_template('index.html', titulo='Pagina Principal', nome='Tiago Chaves')
 
-if __name__ == "__main__":
-    app.run()
+
+app.run(debug=True)
